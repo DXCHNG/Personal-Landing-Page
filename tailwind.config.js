093c902cf72d44e090814d9de736f9b3d2b009c7
Dammy <oselusidamilolaa@gmail.com>
2026-06-23 12:04:21 -0700

    Started building Landing page

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/**@type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        accent: "#22D3EE",
+        surface: "#161B22",
+        dark: "#0D1117",
+      },
+    },
+  },
+  plugins: [],
+};
